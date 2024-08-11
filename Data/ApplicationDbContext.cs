7fbf617f3204aa8ecc979bc59047eca37b931db4
Caio Lagreca <caiomiranda701@hotmail.com>
2024-08-11 15:08:56 +1000

    adding database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityAuthentication.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
